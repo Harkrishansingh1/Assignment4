2ee885fbe0f9248dda048b14f4586ef3173e39c7
Harkrishan Singh <732442@student.tor.loc>
2019-04-03 12:06:32 -0400

    Revert

--- c0732442/c0732442/Program.cs
+++ c0732442/c0732442/Program.cs
@@ -10,7 +10,7 @@ namespace c0732442
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("I will revert");
         }
     }
 }
